ae56ebb8d8a02320204cd38033c62f16c867097b
sergey-ado <adonisiy2@gmail.com>
2026-02-19 23:17:55 +0400

    feat: add prisma

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ import { createServer } from 'node:http';
 // import { fileURLToPath } from 'node:url';
 import { Server } from 'socket.io';
 import { parserInputItem } from './models/parsers.js';
-import { items } from './models/item.js';
+// import { items } from './models/item.js';
 import { v4 as uuid } from 'uuid';
-// import { prisma } from './prisma/prisma.js';
+import { prisma } from './prisma/prisma.js';
 
 const app = express();
 const server = createServer(app);
@@ -20,42 +20,49 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello');
 });
 
-app.post('/users', (req: Request, res: Response) => {
+app.post('/users', async (req: Request, res: Response) => {
   const body: unknown = req.body;
   const inputItem = parserInputItem(body);
   if (inputItem) {
-    const id = uuid();
-    const item = { id, name: inputItem.name };
-    res.status(201).json(item);
-    items.push(item);
-
     // const id = uuid();
-    // console.log(inputItem);
-    // const newUser = await prisma.user.create({
-    //   data: {
-    //     id,
-    //     name: inputItem.name,
-    //   },
-    // });
-    // res.json(newUser);
+    // const item = { id, name: inputItem.name };
+    // res.status(201).json(item);
+    // items.push(item);
+
+    const id = uuid();
+    console.log(inputItem);
+    const newUser = await prisma.user.create({
+      data: {
+        id,
+        name: inputItem.name,
+      },
+    });
+    res.json(newUser);
     // res.json(inputItem);
   } else {
     res.sendStatus(204);
   }
 });
 
-app.get('/users', (req: Request, res: Response) => {
-  // const users = await prisma.user.findMany();
-  // console.log(users);
-  // res.json(users);
-  res.json(items);
+app.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  console.log(users);
+  res.json(users);
+  // res.json(items);
 });
 
-app.get('/users/:id', (req: Request, res: Response) => {
+app.get('/users/:id', async (req: Request, res: Response) => {
   const id = req.params.id;
-  const user = items.find((item) => item.id === id);
-  if (user) res.json(user);
-  else res.sendStatus(404);
+  // const user = items.find((item) => item.id === id);
+  // if (user) res.json(user);
+  // else res.sendStatus(404);
+  if (typeof id === 'string') {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+    if (user) res.json(user);
+    else res.sendStatus(404);
+  } else res.sendStatus(404);
 });
 
 io.on('connection', (socket) => {
